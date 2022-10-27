771477d9a1042d2b45dd5021016f92ccf97009a1
NigarAdigozelova <72kwbg1@code.edu.az>
2022-10-27 23:53:58 +0400

    created task3

---
+++ smallest3.js
@@ -0,0 +1,10 @@
+let a=5;
+b=6;
+c=10;
+if(a<b && a<c){
+    console.log("a is a smallest number");
+}else if( b<a && b<c){
+    console.log("b is a smallest number");
+}else if( c<a && c<b){
+    console.log("c is a smallest number");
+}
